a72f876818522977569eeea258766b99f01ab10c
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2020-10-28 19:42:35 -0400

    Using cypress-axe to check color contrast of the app.

--- cypress/integration/page.js
+++ cypress/integration/page.js
@@ -1,17 +1,19 @@
 describe("page render", () => {
-  // beforeEach(() => {
-  //   cy.visit("http://localhost:3000/");
-  //   cy.injectAxe();
-  // });
-
-  it("renders the page accessibly", () => {
-    cy.visit("http://localhost:3000/").injectAxe();
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+    cy.injectAxe();
   });
+
   it("focuses on the wrapper when you click on the button toggle", () => {
-    cy.visit("http://localhost:3000/")
-      .get("#color-scheme")
+    cy.get("#color-scheme")
       .click()
       .focused()
       .should("have.id", "radio-wrapper");
+  });
+
+  it("should have proper contrast", () => {
+    cy.checkA11y("body", {
+      runOnly: ["cat.color"],
+    });
   });
 });
